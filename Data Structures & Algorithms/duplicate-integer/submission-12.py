96b2420463ec179c11530246397bb244e8e32c43
Diego S <146607267+Diego-GTA@users.noreply.github.com>
2026-04-29 12:48:17 -0400

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-12.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        my_set = {1}
+        my_set.pop()
+        for i in nums:
+            my_set.add(i)
+        if len(my_set) == len(nums):
+            return False
+        else:
+            return True
---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-12.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count = 0
+        maxcount = 0
+        for i in range(len(nums)):
+            if nums[i-1]==0:
+                count = 0
+            if nums[i] == 1:
+                count +=1
+            if count >= maxcount:
+                maxcount = count
+        return maxcount
+
